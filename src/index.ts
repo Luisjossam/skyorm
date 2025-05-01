96a750acdf2c9083c29321bb2e06f0bfb89e51f9
luisjossam <luisjossam@outlook.com>
2025-05-01 16:16:35 -0600

    Fixed index.js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import Model from "./model";
-import Database from "./database";
-=======
 import Model from "./models/Model";
 import Database from "./database/database";
->>>>>>> cfcdf2a (Refactored code)
 
 export { Database, Model };
