841433d8490a0643f11c074a72630d6fe1a0d9d3
luisjossam <luisjossam@outlook.com>
2025-05-02 11:38:11 -0600

    Start transactions class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Model from "./models/Model";
 import Database from "./database/database";
+import Transaction from "./models/Transaction";
 
-export { Database, Model };
+export { Database, Model, Transaction };
